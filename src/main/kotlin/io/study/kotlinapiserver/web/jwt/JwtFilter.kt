e8323106ebdfa6a2cbf4d9c8b0070e3399003992
이윤선 <yoonseon3@gmail.com>
2024-07-12 23:05:05 +0900

    Fix: 누락된 filterChain.doFilter 적용

--- src/main/kotlin/io/study/kotlinapiserver/web/jwt/JwtFilter.kt
+++ src/main/kotlin/io/study/kotlinapiserver/web/jwt/JwtFilter.kt
@@ -34,11 +34,12 @@ class JwtFilter(
         validationToken(request, jwt)
 
         if (StringUtils.hasText(jwt) && jwtProvider.validateAccessToken(jwt)) {
-            val authentication = jwtProvider.getAuthentication(jwt);
+            val authentication = jwtProvider.getAuthentication(jwt)
             log.info("인증정보 저장 {}", authentication);
             SecurityContextHolder.getContext().authentication = authentication
         }
 
+        filterChain.doFilter(request, response)
     }
 
     private fun validationToken(request: HttpServletRequest, token: String) {
@@ -47,30 +48,30 @@ class JwtFilter(
         } catch (e : Exception ) {
             when (e) {
                 is SecurityException, is MalformedJwtException -> {
-                    log.info("잘못된 JWT 서명입니다.");
+                    log.info("잘못된 JWT 서명입니다.")
                     request.setAttribute(ATTRIBUTE, AuthErrorCode.INVALID_JWT_SIGNATURE)
                 }
                 is ExpiredJwtException -> {
-                    log.info("만료된 JWT 토큰입니다.");
+                    log.info("만료된 JWT 토큰입니다.")
                     request.setAttribute(ATTRIBUTE, AuthErrorCode.EXPIRED_JWT_TOKEN);
                 }
                 is UnsupportedJwtException -> {
-                    log.info("지원되지 않는 JWT 토큰입니다.");
+                    log.info("지원되지 않는 JWT 토큰입니다.")
                     request.setAttribute(ATTRIBUTE, AuthErrorCode.UNSUPPORTED_JWT_TOKEN)
                 }
                 is IllegalArgumentException -> {
-                    log.error("JWT 토큰이 잘못되었습니다.");
+                    log.error("JWT 토큰이 잘못되었습니다.")
                     request.setAttribute(ATTRIBUTE, AuthErrorCode.INVALID_JWT_TOKEN)
                 }
                 else -> {
-                    log.error("================================================");
-                    log.error("JwtFilter - doFilterInternal() 오류발생");
-                    log.error("token : {}", token);
-                    log.error("Exception Message : {}", e.message);
-                    log.error("Exception StackTrace : {");
-                    e.printStackTrace();
-                    log.error("}");
-                    log.error("================================================");
+                    log.error("================================================")
+                    log.error("JwtFilter - doFilterInternal() 오류발생")
+                    log.error("token : {}", token)
+                    log.error("Exception Message : {}", e.message)
+                    log.error("Exception StackTrace : {")
+                    e.printStackTrace()
+                    log.error("}")
+                    log.error("================================================")
                     request.setAttribute(ATTRIBUTE, AuthErrorCode.JWT_UNKNOWN_ERROR)
                 }
             }
@@ -78,13 +79,15 @@ class JwtFilter(
     }
 
     private fun resolveToken(request: HttpServletRequest): String {
-        val bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION)
-        if (isExistBearer(bearerToken)) {
-            return bearerToken.substring(TOKEN_PREFIX.length).trim()
+        val bearerToken: String? = request.getHeader(HttpHeaders.AUTHORIZATION)
+        bearerToken.let {
+            if (isExistBearer(bearerToken)) {
+                return bearerToken!!.substring(TOKEN_PREFIX.length).trim()
+            }
         }
         return ""
     }
 
-    private fun isExistBearer(bearerToken: String) =
-        StringUtils.hasText(bearerToken) && bearerToken.startsWith(TOKEN_PREFIX)
+    private fun isExistBearer(bearerToken: String?) =
+        StringUtils.hasText(bearerToken) && bearerToken!!.startsWith(TOKEN_PREFIX)
 }
