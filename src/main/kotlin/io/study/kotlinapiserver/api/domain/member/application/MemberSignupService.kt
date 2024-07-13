2c30790d5b9b1858c2852e868974491520b1d597
이윤선 <yoonseon3@gmail.com>
2024-07-14 00:56:10 +0900

    Feature: 회원가입 메일전송 구현

--- src/main/kotlin/io/study/kotlinapiserver/api/domain/member/application/MemberSignupService.kt
+++ src/main/kotlin/io/study/kotlinapiserver/api/domain/member/application/MemberSignupService.kt
@@ -3,6 +3,7 @@ package io.study.kotlinapiserver.api.domain.member.application
 import io.study.kotlinapiserver.api.domain.member.domain.MemberDomainService
 import io.study.kotlinapiserver.api.domain.member.domain.dto.request.MemberSignupRequest
 import io.study.kotlinapiserver.api.domain.member.domain.dto.response.MemberSignupResponse
+import io.study.kotlinapiserver.infra.mail.MailService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -10,11 +11,24 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class MemberSignupService(
     private val memberDomainService: MemberDomainService,
+    private val mailService: MailService,
 ) {
 
     @Transactional
     fun signup(command: MemberSignupRequest): MemberSignupResponse {
-        return memberDomainService.register(command)
+        /** 회원가입 **/
+        val savedInfo = memberDomainService.register(command)
+
+        /** 이메일 전송 **/
+        val registeredEmail = savedInfo.email
+        val toEmail = toEmailArray(registeredEmail);
+        mailService.sendMail(toEmail, "회원가입 완료 안내", "회원가입이 완료되었습니다.")
+
+        return savedInfo
+    }
+
+    private fun toEmailArray(vararg emails: String): Array<out String> {
+        return emails
     }
 
 }
