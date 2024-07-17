27b724c1f465989ec405f6ece2a6fc0251575f1b
이윤선 <yoonseon3@gmail.com>
2024-07-17 17:18:09 +0900

    Chore: @ValidEmail 어노테이션 추가

--- src/main/kotlin/io/study/kotlinapiserver/api/domain/member/ui/dto/request/PostMemberSigninRequest.kt
+++ src/main/kotlin/io/study/kotlinapiserver/api/domain/member/ui/dto/request/PostMemberSigninRequest.kt
@@ -1,6 +1,11 @@
 package io.study.kotlinapiserver.api.domain.member.ui.dto.request
 
+import io.study.kotlinapiserver.web.annotation.ValidEmail
+
 data class PostMemberSigninRequest(
+
+    @field:ValidEmail
     val email: String,
+
     val password: String,
 )
